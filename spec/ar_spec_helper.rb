f09220ebe9097ba0837676503da2c751dfa73020
slowjack2k <shad0wrunner@gmx.de>
2014-01-27 11:06:01 +0100

    spec setup for integration tests

---
+++ spec/ar_spec_helper.rb
@@ -0,0 +1,33 @@
+require 'sqlite3'
+require 'active_record'
+require 'database_cleaner'
+
+db_dir = File.join(File.expand_path(__dir__ ), "ar_support/db/")
+
+ActiveRecord::Base.establish_connection({
+                                            pool: 5,
+                                            timeout: 5000,
+                                            database: ":memory:",
+                                            adapter: "sqlite3"
+                                        })
+
+ActiveRecord::Migrator.migrate "#{db_dir}/migrate"
+
+Dir[File.join(File.expand_path(__dir__ ), "ar_support/models/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
