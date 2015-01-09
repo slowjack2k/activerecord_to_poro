c72cf415abc4432ceba8d13bca811e44ea30f471
slowjack2k <shad0wrunner@gmx.de>
2015-01-09 10:36:03 +0100

    fixed lock_version default for rails 4.2, fixed attributes assignment for rails 4.2

--- spec/ar_spec_helper.rb
+++ spec/ar_spec_helper.rb
@@ -1,18 +1,28 @@
 require 'active_record'
-require 'database_cleaner'
 
 db_dir = File.join(File.expand_path(__dir__ ), "ar_support/db/")
 # ENV['DATABASE_URL']="sqlite3://localhost/:memory:?pool=5&timeout=5000"
 
+con_settings = {
+    pool: 5,
+    timeout: 5000,
+    database: ":memory:",
+    adapter: "sqlite3"
+}
 
+# con_settings = {
+#     adapter: 'postgresql',
+#     encoding: 'utf8',
+#     pool: 5,
+#     host: 'localhost',
+#     port: 5000,
+#     database: 'poro_test'
+# }
 
-ActiveRecord::Base.establish_connection({
-                                            pool: 5,
-                                            timeout: 5000,
-                                            database: ":memory:",
-                                            adapter: "sqlite3"
-                                        })
+require 'database_cleaner'
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.establish_connection(con_settings)
 ActiveRecord::Migrator.migrate "#{db_dir}/migrate"
 
 Dir[File.join(File.expand_path(__dir__ ), "ar_support/models/**/*.rb")].each { |f| require f }
@@ -20,8 +30,8 @@ Dir[File.join(File.expand_path(__dir__ ), "ar_support/models/**/*.rb")].each { |
 RSpec.configure do |config|
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :truncation, {:except => %w[schema_migrations public.schema_migrations]}
+    DatabaseCleaner.clean_with(:truncation, {:except => %w[schema_migrations public.schema_migrations]})
   end
 
   config.before(:each) do
